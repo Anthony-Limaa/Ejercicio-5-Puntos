5c3e0789a445c2f70d2cc172cd4030fead45e4e1
Anthony Lima <Anthony Lima@TONYPC>
2023-04-01 09:45:23 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio, 5 Puntos/Program.cs
@@ -0,0 +1,25 @@
+﻿void matriz()
+{
+    string[] nombre = new string[]
+    {
+        "Anderson", "Brandon", "Yeimi", "Fatima", "Vivian"
+    };
+
+
+    int[] notas = new int[]
+    {
+        50,90,85,73,95
+    };
+    int mayor = 0;
+    for (int i = 0; i < notas.Length; i++)
+    {
+        if (notas[i] > mayor)
+        {
+            mayor = notas[i];
+
+        }
+    }
+    Console.WriteLine("La nota mayor es de: " + mayor);
+
+}
+matriz();
